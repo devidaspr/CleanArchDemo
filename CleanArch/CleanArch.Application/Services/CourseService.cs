abb2a411fcc09fba340adc372ee4e4d966f12603
devidas <devidasp@hotmail.com>
2019-12-27 04:12:54 -0800

    Commit implemetation of Service and Repository GetCourses methods

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRespository;
+
+        public CourseService(ICourseRepository courseRespository)
+        {
+            this._courseRespository = courseRespository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel ()
+            {
+                Courses = _courseRespository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _dataContext;
+
+        public CourseRepository(UniversityDBContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _dataContext.Courses;
+        }
+    }
+}
